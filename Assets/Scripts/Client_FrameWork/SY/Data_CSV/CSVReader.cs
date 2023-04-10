7cf0eba02ea5681ccf715b9a613e18161006ed0c
nosoeugyub <74639167+nosoeugyub@users.noreply.github.com>
2023-04-11 00:27:36 +0900

    CSV reader Path 경로 설정
    1. 안드로이드
    2. pc
    3. ios

--- Assets/Scripts/Client_FrameWork/SY/Data_CSV/CSVReader.cs
+++ Assets/Scripts/Client_FrameWork/SY/Data_CSV/CSVReader.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CSVReader : MonoBehaviour
+public class CSVReader 
 {
-    // Start is called before the first frame update
-    void Start()
+   
+    protected static string getBasetPath()
     {
-        
+#if UNITY_EDITOR
+        return Application.dataPath + "/Resources/";
+#elif UNITY_ANDROID
+            return Application.persistentDataPath+_filename;
+#elif UNITY_IPHONE
+            return Application.persistentDataPath+"/"+_filename;
+#else
+            return Application.dataPath +"/"+_filename;
+#endif
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
